c3141901fcfde35f6ef18532cbe9cc8c72b8504b
Lucas Delboni <lucas_cd460@hotmail.com>
2016-04-13 01:00:47 +0000

    pagina inicial agora é para o uso do usuario

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script language="JavaScript">
+            function Enviar(opc){
+                if(opc == 0){
+                    document.form.action = "temporadas.php";
+                }
+                if(opc == 1){
+                    document.form.action = "enviar.php";
+                    document.form.target = "";
+                    document.form.submit();
+                }
+            }
+        </script>
+    </head>
+    <body>
+        <h1>consultar uma serie</h1>
+        <form action="" method="get" id="form1" name="form1">
+            Nome da serie: <input type="text" name="nome"><br>
+            <input type="submit" value="temporadas" onclick="document.form1.action='serie/temporadas.php';" />
+            <input type="submit" value="episodios" onclick="document.form1.action='serie/episodios.php';" />
+        </form> 
+        <h1>consultar distribuicao de notas em uma temporada</h1>
+        <form action="distribuicao-temporada.php" method="get" id="form2" name="form2">
+            Nome da serie: <input type="text" name="nome"><br>
+            Temporada: <input type="number" name="temporada"><br>
+            <input type="submit" value="consultar"/>
+        </form> 
+        
+    </body>
+</html>
